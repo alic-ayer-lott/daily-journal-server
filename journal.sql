b109de373f5c6fca0658ce4a4c4c66ca7687f09e
Alicia AyersLott <amayers92@gmail.com>
2021-10-18 17:21:57 -0500

    attempting to create get all function

--- journal.sql
+++ journal.sql
@@ -11,3 +11,13 @@ CREATE TABLE `Mood` (
     `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     `label`    TEXT NOT NULL,
 )
+
+
+INSERT INTO `Entries` VALUES (null, "Python", "I feel like I am getting the hang of this.", "10/18/2021", 1);
+INSERT INTO `Entries` VALUES (null, "SQL", "I like using SQL.", "10/15/2021", 2);
+INSERT INTO `Entries` VALUES (null, "SQL", "It is making data easier to understand.", "10/13/2021", 3);
+
+INSERT INTO `Mood` VALUES (1, "Happy");
+INSERT INTO `Mood` VALUES (2, "Excited");
+INSERT INTO `Mood` VALUES (3, "Growth Mindset");
+INSERT INTO `Mood` VALUES (4, "Sad")
---
+++ models/entry.py
@@ -0,0 +1,7 @@
+class Entry():
+
+    def __init__(self, id, entry, date, mood_id):
+        self.id = id
+        self.entry = entry
+        self.date = date
+        self.mood_id = mood_id
